5b527659090ff4be4fb85022f85ea00cdcb96286
wanxiong <xiong.wan@team-lab.cn>
2017-02-24 10:05:43 +0800

    更新

--- README-utils.js
+++ README-utils.js
@@ -11,8 +11,6 @@
 		email:/^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(.[a-zA-Z0-9_-])+/,
 		bankCard : /^(\d{16,19})$/,
 
-
-
 		/* 华丽的分割线 ===================================                   一般验证     ============================================ */
 
 
@@ -437,9 +435,81 @@
 			        callback(baseUrl);
 			     }
 		    
+		},
+		//写cookies 
+		setCookie:function(name,value) { 
+		    var days = 1; 
+		    var exp = new Date(); 
+		    exp.setTime(exp.getTime() + days*24*60*60*1000); 
+		    document.cookie = name + "="+ escape (value) + ";expires=" + exp.toGMTString(); 
+		}, 
+
+		//读取cookies 
+		readCookie: function (name) { 
+		    var arr,reg=new RegExp("(^| )"+name+"=([^;]*)(;|$)");
+		    if(arr=document.cookie.match(reg)){
+		        return unescape(arr[2]); 
+		    }else{
+		        return null;
+		    }
+		        
+		} ,
+
+		//删除cookies 
+		delCookie:function (name) { 
+		    var cval=readCookie(name); 
+		    if(cval!=null){
+		        document.cookie= name + "="+cval+";expires="+(new Date(0)).toGMTString();
+		    }
+		},
+		//数组功能扩展
+		each: function(fn){
+		    fn = fn || Function.K;
+		     var a = [];
+		     var args = Array.prototype.slice.call(arguments, 1);
+		     for(var i = 0; i < this.length; i++){
+		         var res = fn.apply(this,[this[i],i].concat(args));
+		         if(res != null) a.push(res);
+		     }
+		     return a;
+		},
+
+		//数组是否包含指定元素
+		contains : function(suArr){
+		    for(var i = 0; i < this.length; i ++){
+		        if(this[i] == suArr){
+		            return true;
+		        }
+		     }
+		     return false;
+		},
+		//不重复元素构成的数组
+		uniquelize : function(){
+		     var ra = new Array();
+		     for(var i = 0; i < this.length; i ++){
+		        if(!ra.contains(this[i])){
+		            ra.push(this[i]);
+		        }
+		     }
+		     return ra;
+		},
+		//两个数组的补集
+		complement : function(a, b){
+		     return Array.minus(Array.union(a, b),Array.intersect(a, b));
+		},
+		//两个数组的交集
+		intersect : function(a, b){
+		     return a.uniquelize().each(function(o){return b.contains(o) ? o : null});
+		},
+		//两个数组的差集
+		minus : function(a, b){
+		     return a.uniquelize().each(function(o){return b.contains(o) ? null : o});
+		},
+		//两个数组并集
+		union : function(a, b){
+		     return a.concat(b).uniquelize();
 		}
-
-
+		
 		
 }
 	//module.exports = util;
